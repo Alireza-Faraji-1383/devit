e69a2f2c37d08a77bcc371b2f970344d4295aa8b
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-11 16:58:19 +0330

    add media public

--- AAA/urls.py
+++ AAA/urls.py
@@ -1,11 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
-     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
