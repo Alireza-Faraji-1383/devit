5f3bf21aa84b020f7883dbd08b0a16b349344aa3
Alireza-Faraji-1383 <farajialireza664@gmail.com>
2025-06-16 12:57:19 +0330

    fix urls Post

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import PostsView, PostCreateView , PostView  , UserPostsView
 
 urlpatterns = [
    path('', PostsView.as_view(), name='posts'),
-   path('<str:slug>/', PostView.as_view(), name='post'),
-   path('<str:user>/' , UserPostsView.as_view(), name='user_posts'),
    path('create/' , PostCreateView.as_view(), name='post_create'),
+   path('user/<str:user>/' , UserPostsView.as_view(), name='user_posts'),
+   path('<str:slug>/', PostView.as_view(), name='post'),
 ]
