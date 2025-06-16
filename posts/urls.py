dcdd2485ebc0871c40ae7ef53cab45f1b416c0a8
Alireza-Faraji-1383 <farajialireza664@gmail.com>
2025-06-16 12:26:56 +0330

    fix slug

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import PostsView, PostCreateView , PostView  , UserPostsView
 
 urlpatterns = [
    path('', PostsView.as_view(), name='posts'),
-   path('<int:id>/', PostView.as_view(), name='post'),
+   path('<str:slug>/', PostView.as_view(), name='post'),
    path('<str:user>/' , UserPostsView.as_view(), name='user_posts'),
    path('create/' , PostCreateView.as_view(), name='post_create'),
 ]
